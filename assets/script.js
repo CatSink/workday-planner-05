6f3ac82da4ba02e00fb2ccdfc20722c805185172
CatSink <raindropwebweaver@live.com>
2023-02-27 22:10:18 -0500

    added save-btn click event fr user storage, added current hour comparison function to change bg-color of time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,25 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+setInterval(function() {
+  $('#time-display').text(dayjs().format('MMM D YYYY, h:mm:ss'));
+}, 1000);
+function updateClock() {
+  //compares current hour to block time in the workday planner.
+}
+let currentHour= dayjs().hour();
+  $("#hour").each(function() {
+    let hour = parseInt($(this).attr("id").split("-")[1]);
+    if (hour < currentHour)$("p").css("background-color", "green");
+    if (hour > currentHour)$("p").css("background-color", "grey"); 
+    if (hour == currentHour)$("p").css("background-color", "red");
+  })
+$(".saveBtn").on("click", function(){
+  var value = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+    localStorage.setItem(time, value);
+})
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +38,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
